262e155a022fe10faf6858244c99f86e2213d58f
jack yao <yaowenqiang111@163.com>
2015-06-14 09:53:10 +0800

    send data to view using the compact function.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -19,7 +19,8 @@ class PagesController extends Controller
         ];
         $name = "Jacky yao";
         $name2 = "<em>i am a tag wrapped string</em>";
-        return view('pages.home',['lessons'=>$lessons,'name'=>$name,'name2'=>$name2]);
+        //return view('pages.home',['lessons'=>$lessons,'name'=>$name,'name2'=>$name2]);
+        return view('pages.home',compact('lessons','name','name2'));
     }
 
     public function about()
