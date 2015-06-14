d4e65b84c2a22e7744ae57d9a5d4f4ba4c18ad56
jack yao <yaowenqiang111@163.com>
2015-06-14 18:38:57 +0800

    change the edit and create template to partial

--- resources/views/songs/create.blade.php
+++ resources/views/songs/create.blade.php
@@ -3,21 +3,7 @@
 @section('content')
 <h2>Add a new song.</h2>
 {!! Form::open(['route'=>'songs.store']) !!}
-<div class="form-group">
-{!! Form::Label("title","title:") !!}
-{!! Form::text('title',null,['class'=>'form-control']) !!}
-</div>
-<div class="form-group">
-{!! Form::Label("slug","Slug:") !!}
-{!! Form::text('slug',null,['class'=>'form-control','placeholder'=>'Enter a slug']) !!}
-</div>
-<div class="form-group">
-{!! Form::Label("lyrics","lyrics:") !!}
-{!! Form::textarea('lyrics',null,['class'=>'form-control']) !!}
-</div>
-<div class="form-group">
-{!! Form::submit('Create Song',['class'=>'btn btn-primary']) !!}
-</div>
+@include('songs.form')
 {!! Form::close() !!}
 @stop
 
--- resources/views/songs/edit.blade.php
+++ resources/views/songs/edit.blade.php
@@ -3,18 +3,6 @@
 @section('content')
 <h2> {{ $song->title }}</h2>
 {!! Form::model($song,['route'=>['songs.update',$song->slug],'method'=>'PATCH']) !!}
-<div class="form-group">
-{!! Form::text('title',null,['class'=>'form-control']) !!}
-</div>
-<div class="form-group">
-{!! Form::text('slug',null,['class'=>'form-control','placeholder'=>'Enter a slug']) !!}
-</div>
-{!! Form::model($song) !!}
-<div class="form-group">
-{!! Form::textarea('lyrics',null,['class'=>'form-control']) !!}
-</div>
-<div class="form-group">
-{!! Form::submit('Update Song',['class'=>'btn btn-primary']) !!}
-</div>
+@include('songs.form')
 {!! Form::close() !!}
 @stop
